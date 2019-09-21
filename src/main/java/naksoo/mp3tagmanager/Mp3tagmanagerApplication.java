6ad53321d2948bc2a6610831314ded8e97dece6c
choinaksoo <cns@innogrid.com>
2019-09-21 22:26:14 +0900

    1. init

---
+++ src/main/java/naksoo/mp3tagmanager/Mp3tagmanagerApplication.java
@@ -0,0 +1,13 @@
+package naksoo.mp3tagmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3tagmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mp3tagmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/naksoo/mp3tagmanager/Mp3tagmanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package naksoo.mp3tagmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp3tagmanagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
